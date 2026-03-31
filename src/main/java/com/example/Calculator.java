ec24071e10a4692c7cabad7cd0e0f571e095ede8
ssavindu <ssavindu@gmail.com>
2026-03-31 14:11:09 +0530

    introduction quality issues

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -4,27 +4,33 @@ public class Calculator {
 
     // Code Smell: Long method + high complexity
     public int calculate(int a, int b, String op) {
-
-        if(op.equals("add")) {
-            return a + b;
-        }
-        if(op.equals("sub")) {
-            return a - b;
-        }
-        if(op.equals("mul")) {
-            return a * b;
-        }
-        if(op.equals("div")) {
-            if(b == 0) {
-                return 0;
-            }
-            return a / b;
-        }
-        if(op.equals("mod")) {
-            return a % b;
-        }
-
-        return 0;
+if(op.equals("add")) {
+return a + b;
+} else if(op.equals("add-again")) {
+return a + b; // DUPLICATION
+} else if(op.equals("sub")) {
+return a - b;
+} else if(op.equals("sub-again")) {
+return a - b; // DUPLICATION
+} else if(op.equals("mul")) {
+return a * b;
+} else if(op.equals("div")) {
+if(b == 0) {
+return 0;
+} else {
+return a / b;
+}
+} else if(op.equals("mod")) {
+return a % b;
+} else if(op.equals("pow")) {
+int result = 1;
+for(int i = 0; i < b; i++) {
+result = result * a;
+}
+return result;
+} else {
+return 0;
+}
     }
 
     // Code Duplication (students must remove)
@@ -35,4 +41,9 @@ public class Calculator {
     public int sumValues(int a, int b) {
         return a + b;
     }
+    // INTENTIONAL DUPLICATION
+    public int addAgain(int a, int b) {
+        return a + b;
+    }
+
 }
